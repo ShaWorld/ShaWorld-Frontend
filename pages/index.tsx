52c0f6b84c732206b067a3f9868c2883df387327
Sonwanseo <sonwanseo@gmail.com>
2021-03-23 22:38:41 +0900

    <index.tsx> remove not working code

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,5 @@ import React from 'react';
 import SignIn from '../components/SignIn/SignIn';
 
 export default function Home() {
-    return (
-        <React.Fragment>
-            <style jsx global>{`
-                html, body, #__next {
-                    width: 100%;
-                    height: 100vh;
-                    margin: 0;
-                    display: flex;
-                    justify-content: center
-                }`}
-            </style>
-            <SignIn />
-        </React.Fragment>
-    )
+    return <SignIn />
 }
