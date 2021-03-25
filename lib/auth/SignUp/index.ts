a6f0574cbe070900f48b8be466be099f94889cef
Sonwanseo <sonwanseo@gmail.com>
2021-03-26 01:31:00 +0900

    <lib/SignUp> remove error handling

--- lib/auth/SignUp/index.ts
+++ lib/auth/SignUp/index.ts
@@ -3,43 +3,14 @@ import { BASE_URL } from "../../BASE";
 import { SignUpRequest } from "../../payloads/SignUp";
 
 export const signUp = async (data: SignUpRequest) => {
-  try {
-    return await axios
-      .post(BASE_URL + "/user/signup", {
-        email: data.email,
-        nickname: data.nickname,
-        password: data.password,
-        passwordConfirm: data.passwordConfirm,
-      })
-      .then((result) => {
-        if (result.status == 201) {
-          console.log("modal on");
-        }
-      });
-  } catch (e) {
-    switch (e.response.status) {
-      case 400: {
-        window.alert("잘못된 형식입니다.");
-        location.reload();
-        break;
-      }
-      case 409: {
-        switch (e.response.data.code) {
-          case "EMAIL_DUPLICATION":
-            window.alert("이미 사용중인 이메일입니다.");
-            location.reload();
-            break;
-          case "NICKNAME_DUPLICATION":
-            window.alert("이미 사용중인 닉네임입니다.");
-            location.reload();
-            break;
-          default:
-            console.log("알 수 없는 오류");
-        }
-        break;
-      }
-      default:
-        console.log("알 수 없는 오류");
-    }
-  }
+  return await axios
+    .post(BASE_URL + "/user/signup", {
+      email: data.email,
+      nickname: data.nickname,
+      password: data.password,
+      passwordConfirm: data.passwordConfirm,
+    })
+    .then((res) => {
+      return res;
+    });
 };
