f153bffc3ef2a228b086177a41e123722f601509
Sonwanseo <sonwanseo@gmail.com>
2021-03-26 01:32:16 +0900

    <signup.tsx> prettier

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import SignUp from '../components/SignUp/SignUp';
+import React from "react";
+import SignUp from "../components/SignUp/SignUp";
 
 export default function signup() {
-    return <SignUp />
+  return <SignUp />;
 }
