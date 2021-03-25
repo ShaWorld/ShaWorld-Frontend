ebccacf4ebb775666755af1dbf709695876b75a6
Sonwanseo <sonwanseo@gmail.com>
2021-03-26 01:52:02 +0900

    <utils/api/signup/payload> remove no use payloads

--- utils/api/signup/payload/index.ts
+++ utils/api/signup/payload/index.ts
@@ -1,9 +1,5 @@
 import { ResDefault } from "../../BASE";
 
-export const PASSWORD_MISMATCH = "비밀번호와 비밀번호 재확인이 일치하지 않습니다." as const;
-export const EMAIL_DUPLICATION = "이미 사용중인 이메일입니다." as const;
-export const NICKNAME_DUPLICATION = "이미 사용중인 닉네임입니다." as const;
-
 export interface errorObj {
   emailError: boolean;
   nicknameError: boolean;
