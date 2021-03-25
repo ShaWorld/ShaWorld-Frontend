1755942fd573ea023b0bcd0a7b595889165d39ae
Sonwanseo <sonwanseo@gmail.com>
2021-03-26 01:46:48 +0900

    <utils/api/signup/payload> change import path

--- utils/api/signup/payload/index.ts
+++ utils/api/signup/payload/index.ts
@@ -1,4 +1,4 @@
-import { ResDefault } from "..";
+import { ResDefault } from "../../BASE";
 
 export const PASSWORD_MISMATCH = "비밀번호와 비밀번호 재확인이 일치하지 않습니다." as const;
 export const EMAIL_DUPLICATION = "이미 사용중인 이메일입니다." as const;
