fd5a603fb3c2ac0d6b0f93ab74867b7496d4f586
Sonwanseo <sonwanseo@gmail.com>
2021-03-27 00:11:24 +0900

    <pages/_app.tsx> add modal in routing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/app";
 import { rootStore } from "../modules/store";
 import { Provider } from "react-redux";
+import Modal from "../components/Modal";
 import "../styles/globals.css";
 
 const store = rootStore();
@@ -8,6 +9,7 @@ const store = rootStore();
 function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Modal />
       <Component {...pageProps}></Component>
     </Provider>
   );
