9132e4e917cb8109c95876d8e97e4928480b5432
Sonwanseo <sonwanseo@gmail.com>
2021-03-27 20:39:57 +0900

    <components/Modal/SignUpAlertModal> complete SignUpAlertModal

--- components/Modal/SignUpAlertModal/index.tsx
+++ components/Modal/SignUpAlertModal/index.tsx
@@ -1,11 +1,25 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
+import { useRouter } from "next/router";
+import * as S from "./style";
 
 interface Props {
   deleteModal: () => void;
 }
 
 const SignUpAlertModal: FC<Props> = ({ deleteModal }) => {
-  return <div onClick={deleteModal}>hello</div>;
+  const router = useRouter();
+
+  const confirmSignUp = () => {
+    deleteModal();
+    router.push("/");
+  };
+
+  return (
+    <S.ModalContainer>
+      <S.ModalText>회원가입에 성공하셨습니다.</S.ModalText>
+      <S.ModalConfirmButton onClick={confirmSignUp}>확인</S.ModalConfirmButton>
+    </S.ModalContainer>
+  );
 };
 
 export default SignUpAlertModal;
