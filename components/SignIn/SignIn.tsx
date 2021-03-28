bc68455c13b8fa5cfaec3345caf8f5cd60c640f3
Sonwanseo <sonwanseo@gmail.com>
2021-03-28 22:55:13 +0900

    <components/SignIn> create SignIn component

--- components/SignIn/SignIn.tsx
+++ components/SignIn/SignIn.tsx
@@ -1,21 +1,105 @@
-import React from 'react';
-import * as S from './style';
+import React, { useState, FC } from "react";
+import { useRouter } from "next/router";
+import * as S from "./style";
+import { SignInRequest } from "../../utils/api/signin/payload";
+import { signIn } from "../../utils/api/signin";
 
-const SignIn = () => {
-    return (
-        <S.Container>
-            <S.MainWrapper>
-                <S.Logo>로고</S.Logo>
-                <S.ErrorText>이메일 형식이 올바르지 않습니다.</S.ErrorText>
-                <S.InputBox placeholder="이메일" name="email" autoComplete="off" />
-                <S.InputBox type="password" placeholder="비밀번호" name="password" />
-                <S.Button>로그인</S.Button>
-                <S.DividingLine />
-                <S.Button>회원가입</S.Button>
-            </S.MainWrapper>
-            <S.BottomText>©ShaWorld Inc. All Rights Reserved</S.BottomText>
-        </S.Container>
-    )
-}
+const SignIn: FC = () => {
+  const router = useRouter();
+  const [signInForm, setSignInForm] = useState<SignInRequest>({
+    email: "",
+    password: "",
+  });
+  const [errorText, setErrorText] = useState<string>("");
+
+  const onChangeForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSignInForm({
+      ...signInForm,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  function callFormatCheckFunc(): boolean {
+    return checkEmailFormat() || checkPasswordFormat();
+  }
+
+  function checkEmailFormat(): boolean {
+    if (!signInForm.email) {
+      setErrorText("이메일을 입력해주세요.");
+      return true;
+    } else if (!signInForm.email.includes("@")) {
+      setErrorText("올바른 이메일 형식이 아닙니다.");
+      return true;
+    }
+    setErrorText("");
+    return false;
+  }
+
+  function checkPasswordFormat(): boolean {
+    if (!signInForm.password) {
+      setErrorText("비밀번호를 입력해주세요.");
+      return true;
+    }
+    setErrorText("");
+    return false;
+  }
+
+  function onSubmitSignInForm(): Promise<object> {
+    if (callFormatCheckFunc()) return;
+    signIn(signInForm).then(
+      (res) => {
+        router.push("/main");
+        return Promise.resolve(res);
+      },
+      (err) => {
+        switch (err.response.status) {
+          case 400: {
+            switch (err.response.data.code) {
+              case "NO_MATCH_EMAIL":
+                setErrorText("일치하는 이메일이 없습니다.");
+                break;
+              case "PASSWORD_MISMATCH":
+                setErrorText("비밀번호가 일치하지 않습니다.");
+                break;
+              default:
+                setErrorText("잘못된 형식입니다.");
+                break;
+            }
+            break;
+          }
+          default:
+            location.reload();
+        }
+        return Promise.reject(err.response);
+      }
+    );
+  }
+
+  return (
+    <S.Container>
+      <S.MainWrapper>
+        <S.Logo>로고</S.Logo>
+        <S.ErrorText isError={errorText}>{errorText}</S.ErrorText>
+        <S.InputBox
+          type="text"
+          placeholder="이메일"
+          name="email"
+          autoComplete="off"
+          onChange={onChangeForm}
+        />
+        <S.InputBox
+          type="password"
+          placeholder="비밀번호"
+          name="password"
+          onChange={onChangeForm}
+        />
+        <S.Button onClick={onSubmitSignInForm}>로그인</S.Button>
+        <S.DividingLine />
+        <S.Button onClick={() => router.push("/signup")}>회원가입</S.Button>
+      </S.MainWrapper>
+      <S.BottomText>©ShaWorld Inc. All Rights Reserved</S.BottomText>
+    </S.Container>
+  );
+};
 
 export default SignIn;
--- components/SignIn/style.tsx
+++ components/SignIn/style.tsx
@@ -1,92 +1,93 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 1200px;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  width: 1200px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const MainWrapper = styled.div`
-    width: 300px;
-    height: 500px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  width: 300px;
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Logo = styled.div`
-    width: 200px;
-    height: 100px;
-    background: #4F4646;
-    margin-bottom: 23px;
-    color: #fff;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 30px;
+  width: 200px;
+  height: 100px;
+  background: #4f4646;
+  margin-bottom: 23px;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
 `;
 
 export const ErrorText = styled.p`
-    visibility: hidden;
-    font-size: 13px;
-    color: #FF0000;
-    margin: 0;
+  visibility: ${(props) => (props.isError ? "inherit" : "hidden")};
+  font-size: 13px;
+  color: #ff0000;
+  height: 16px;
+  margin: 0;
 `;
 
 export const InputBox = styled.input`
-    width: 100%;
-    height: 50px;
-    border: 2px solid #4F4646;
-    box-sizing: border-box;
-    outline: none;
-    border-radius: 16px;
-    margin-top: 10px;
-    color: #707070;
-    padding-left: 15px;
-    font-size: 20px;
+  width: 100%;
+  height: 50px;
+  border: 2px solid #4f4646;
+  box-sizing: border-box;
+  outline: none;
+  border-radius: 16px;
+  margin-top: 23px;
+  color: #707070;
+  padding-left: 15px;
+  font-size: 20px;
 
-    &::placeholder {
-        font-size: 25px;
-        line-height: 25px;
-    }
+  &::placeholder {
+    font-size: 25px;
+    line-height: 25px;
+  }
 `;
 
 export const InitPassword = styled.a`
-    width: 280px;
-    text-align: left;
-    font-size: 13px;
-    color: #707070;
-    text-decoration: underline;
-    cursor: pointer;
-    margin: 0;
-    margin-top: 14px;
+  width: 280px;
+  text-align: left;
+  font-size: 13px;
+  color: #707070;
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0;
+  margin-top: 14px;
 `;
 
 export const Button = styled.button`
-    width: 300px;
-    height: 50px;
-    background: #4F4646;
-    color: #fff;
-    font-size: 28px;
-    border-radius: 10px;
-    margin: 20px 0 20px 0;
-    outline: none;
-    border: none;
-    cursor: pointer;
+  width: 300px;
+  height: 50px;
+  background: #4f4646;
+  color: #fff;
+  font-size: 28px;
+  border-radius: 10px;
+  margin: 20px 0 20px 0;
+  outline: none;
+  border: none;
+  cursor: pointer;
 `;
 
 export const DividingLine = styled.div`
-    width: 300px;
-    height: 1px;
-    background: #4F4646;
+  width: 300px;
+  height: 1px;
+  background: #4f4646;
 `;
 
 export const BottomText = styled.p`
-    font-size: 10px;
-    color: #707070;
-    margin: 0;
+  font-size: 10px;
+  color: #707070;
+  margin: 0;
 `;
