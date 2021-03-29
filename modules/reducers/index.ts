999e45e925fc3dc00f0e8b1ca778b0cf0988205e
Sonwanseo <sonwanseo@gmail.com>
2021-03-29 13:56:41 +0900

    <modules/reducers> add userReducer to rootReducer

--- modules/reducers/index.ts
+++ modules/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import modalReducer from "./modal";
+import userReducer from "./user";
 
 const rootReducer = combineReducers({
   modal: modalReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
