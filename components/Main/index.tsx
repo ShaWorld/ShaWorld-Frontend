e5a2a1d13cb691ab244f67ec103fe050f6ab3b1f
Sonwanseo <sonwanseo@gmail.com>
2021-03-29 13:53:47 +0900

    <components/Main> create Main Component

---
+++ components/Main/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC, useEffect } from "react";
+import { useRouter } from "next/router";
+import Header from "../Header";
+
+const Main: FC = () => {
+  // const router = useRouter();
+
+  // useEffect(() => {
+  //   router.push("/mypage");
+  // });
+
+  return <Header />;
+};
+
+export default Main;
