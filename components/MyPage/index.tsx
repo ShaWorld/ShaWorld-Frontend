93bea6fb9227243b1c8c0fff7bbf4798746958bd
Sonwanseo <sonwanseo@gmail.com>
2021-03-29 19:24:12 +0900

    <components/MyPage> publish myPage

--- components/MyPage/index.tsx
+++ components/MyPage/index.tsx
@@ -1,8 +1,48 @@
 import React, { FC } from "react";
+import useUser from "../../utils/hooks/user";
 import Header from "../Header";
+import * as S from "./style";
 
 const MyPage: FC = () => {
-  return <Header />;
+  const {
+    state: { userEmail, userNickname, userProfile },
+  } = useUser();
+
+  return (
+    <S.Container>
+      <Header />
+      <S.Wrapper>
+        <S.Title>마이페이지</S.Title>
+        <S.ContentsWrapper>
+          <S.ImgInput
+            src={
+              userProfile != null
+                ? userProfile
+                : "http://image.auction.co.kr/itemimage/1b/7d/19/1b7d19f331.jpg"
+            }
+          />
+          <S.InfoFormWrapper>
+            <S.InfoWrapper>
+              <S.InfoTitle>닉네임</S.InfoTitle>
+              <S.InfoTextButtonWrapper>
+                <S.InfoText>{userNickname}</S.InfoText>
+                <S.InfoFixButton>수정하기</S.InfoFixButton>
+              </S.InfoTextButtonWrapper>
+            </S.InfoWrapper>
+            <S.InfoWrapper>
+              <S.InfoTitle>이메일</S.InfoTitle>
+              <S.InfoText>{userEmail}</S.InfoText>
+            </S.InfoWrapper>
+            <S.ButtonWrapper>
+              <S.ChangePasswordButton>비밀번호 변경</S.ChangePasswordButton>
+              <S.LogoutButton>로그아웃</S.LogoutButton>
+            </S.ButtonWrapper>
+            <S.SignOutButton>회원 탈퇴</S.SignOutButton>
+          </S.InfoFormWrapper>
+        </S.ContentsWrapper>
+      </S.Wrapper>
+    </S.Container>
+  );
 };
 
 export default MyPage;
---
+++ components/MyPage/style.tsx
@@ -0,0 +1,138 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Wrapper = styled.div`
+  width: 1200px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+export const Title = styled.h1`
+  margin-left: 44px;
+  font-size: 40px;
+  font-weight: bold;
+  color: #807777;
+`;
+
+export const ContentsWrapper = styled.div`
+  width: 1200px;
+  height: 524px;
+  border: 1px solid #4f4646;
+  border-radius: 20px;
+  display: flex;
+`;
+
+export const ImgInput = styled.img`
+  width: 252px;
+  height: 252px;
+  border-radius: 50%;
+  border: 3px solid #4f4646;
+  margin-top: 37px;
+  margin-left: 54px;
+`;
+
+export const InfoFormWrapper = styled.div`
+  margin-left: 63px;
+  margin-top: 42px;
+`;
+
+export const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-bottom: 50px;
+`;
+
+export const InfoTitle = styled.p`
+  font-size: 25px;
+  font-weight: bold;
+  color: #807777;
+  margin: 0;
+`;
+
+export const InfoTextButtonWrapper = styled.div`
+  display: flex;
+`;
+
+export const InfoText = styled.div`
+  width: 300px;
+  height: 50px;
+  border-radius: 16px;
+  border: 2px solid #4f4646;
+  margin-top: 10px;
+  font-size: 20px;
+  color: #807777;
+  padding-left: 13px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const InfoFixButton = styled.button`
+  width: 111px;
+  height: 50px;
+  background: #4f4646;
+  margin-left: 20px;
+  margin-top: 10px;
+  border-radius: 10px;
+  font-size: 20px;
+  color: #fff;
+  border: none;
+  outline: none;
+  cursor: pointer;
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 620px;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ChangePasswordButton = styled.button`
+  width: 300px;
+  height: 50px;
+  color: #fff;
+  background: #4f4646;
+  border-radius: 10px;
+  font-size: 28px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+`;
+
+export const LogoutButton = styled.button`
+  width: 300px;
+  height: 50px;
+  color: #4f4646;
+  background: #fff;
+  border-radius: 10px;
+  border: 1px solid #4f4646;
+  font-size: 28px;
+  outline: none;
+  cursor: pointer;
+`;
+
+export const SignOutButton = styled.button`
+  width: 300px;
+  height: 50px;
+  background: #d7c7a6;
+  color: #fff;
+  border-radius: 10px;
+  font-size: 28px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  margin-top: 50px;
+  margin-left: 505px;
+`;
