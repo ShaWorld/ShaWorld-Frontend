1c7d03cf75d9add8f448aeafe290867e27032144
Sonwanseo <sonwanseo@gmail.com>
2021-03-30 23:23:14 +0900

    <utils/api/mypage> change TOKEN_NAME to Authorization & add changePasswo

--- utils/api/mypage/index.ts
+++ utils/api/mypage/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { BASE_URL, TOKEN_NAME } from "../BASE";
+import { ChangePasswordRequest } from "./payload";
 
 export const changeNickname = async (changedNickname: string) => {
   const res = await axios.patch(
@@ -9,7 +10,24 @@ export const changeNickname = async (changedNickname: string) => {
     },
     {
       headers: {
-        TOKEN_NAME: localStorage.getItem(TOKEN_NAME),
+        Authorization: localStorage.getItem(TOKEN_NAME),
+      },
+    }
+  );
+  return res;
+};
+
+export const changePassword = async (data: ChangePasswordRequest) => {
+  const res = await axios.patch(
+    BASE_URL + "/user/password",
+    {
+      currentPassword: data.currentPassword,
+      changePassword: data.changePassword,
+      changePasswordConfirm: data.changePasswordConfirm,
+    },
+    {
+      headers: {
+        Authorization: localStorage.getItem(TOKEN_NAME),
       },
     }
   );
