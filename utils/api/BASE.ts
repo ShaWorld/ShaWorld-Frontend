05939f2c03734aa269f04e2924c7175f1ebe3153
Sonwanseo <sonwanseo@gmail.com>
2021-03-30 19:01:16 +0900

    <utils/api/BASE> remove no using codes

--- utils/api/BASE.ts
+++ utils/api/BASE.ts
@@ -1,10 +1,3 @@
 export const BASE_URL = "http://localhost:8000";
 
 export const TOKEN_NAME = "Authorization";
-export const TOKEN = "token";
-
-export interface ResDefault {
-  status: number;
-  message: string;
-  code: string;
-}
