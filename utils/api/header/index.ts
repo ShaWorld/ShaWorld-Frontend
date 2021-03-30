020f41596e3828232d0623d687ec9e971946a4bd
Sonwanseo <sonwanseo@gmail.com>
2021-03-30 19:02:05 +0900

    <utils/api/header> change "Authorization" to TOKEN_NAME

--- utils/api/header/index.ts
+++ utils/api/header/index.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
-import { BASE_URL } from "../BASE";
+import { BASE_URL, TOKEN_NAME } from "../BASE";
 import { UserInfoResponse } from "./payload";
 
 export const getUserInfo = async (): Promise<UserInfoResponse> => {
   const { data } = await axios.get<UserInfoResponse>(BASE_URL + "/user", {
     headers: {
-      Authorization: localStorage.getItem("Authorization"),
+      TOKEN_NAME: localStorage.getItem(TOKEN_NAME),
     },
   });
+
   return data;
 };
