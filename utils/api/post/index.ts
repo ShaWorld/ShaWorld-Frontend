0ea45305b21a975a44e64b9ffbe72f423d6e9848
Sonwanseo <sonwanseo@gmail.com>
2021-04-01 23:52:22 +0900

    <utils/api/post> get data from requestParam

--- utils/api/post/index.ts
+++ utils/api/post/index.ts
@@ -5,16 +5,17 @@ import { CreatePostRequest } from "./payload";
 export const createPost = async (data: CreatePostRequest) => {
   var formData = new FormData();
   formData.append("thumbnail", data.thumbnail);
-  formData.append("title", data.title);
-  formData.append("address", data.address);
-  formData.append("detail", data.detail);
-  formData.append("price", data.price.toString());
-  formData.append("date", "2021-04-01");
-  const res = await axios.post(BASE_URL + "/post/create", formData, {
-    headers: {
-      Authorization: localStorage.getItem(TOKEN_NAME),
-    },
-  });
+
+  const res = await axios.post(
+    BASE_URL +
+      `/post/create?title=${data.title}&address=${data.address}&detail=${data.detail}&price=${data.price}&date=2021-04-01`,
+    formData,
+    {
+      headers: {
+        Authorization: localStorage.getItem(TOKEN_NAME),
+      },
+    }
+  );
 
   return res;
 };
