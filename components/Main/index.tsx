662e9dda37f14c79338b5831c304237d6d4291d9
Sonwanseo <sonwanseo@gmail.com>
2021-04-05 23:43:06 +0900

    <components/Main> publish main page

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -1,8 +1,31 @@
 import React, { FC } from "react";
 import Header from "../Header";
+import Post from "../common/Post";
+import * as S from "./style";
 
 const Main: FC = () => {
-  return <Header />;
+  return (
+    <S.Container>
+      <Header />
+      <S.Wrapper>
+        <S.Title>최신 대여 가능 물품</S.Title>
+        <S.PostWrapper>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </S.PostWrapper>
+      </S.Wrapper>
+    </S.Container>
+  );
 };
 
 export default Main;
---
+++ components/Main/style.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Wrapper = styled.div`
+  width: 1200px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const Title = styled.h1`
+  margin: 0;
+  margin-left: 71px;
+  margin-top: 53px;
+  font-size: 30px;
+  font-weight: bold;
+  color: #807777;
+`;
+
+export const PostWrapper = styled.div`
+  width: 1040px;
+  margin-top: 35px;
+  margin-left: 86px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
