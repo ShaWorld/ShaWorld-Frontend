b0c67accad5416ea83e987087ac906762ff728bd
Sonwanseo <sonwanseo@gmail.com>
2021-04-07 14:19:56 +0900

    <utils/api/postdetail/payload> create GetPostDetailInfo interface

---
+++ utils/api/postdetail/payload/index.ts
@@ -0,0 +1,9 @@
+export interface GetPostDetailInfo {
+  postThumbnail: string;
+  postTitle: string;
+  postAuthor: string;
+  postAddress: string;
+  postDetail: string;
+  postPrice: number;
+  postDate: string;
+}
