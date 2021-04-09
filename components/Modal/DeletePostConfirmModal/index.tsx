6af357cf51eb1e8db35100a4e852694b825ea8ff
Sonwanseo <sonwanseo@gmail.com>
2021-04-09 11:19:56 +0900

    <DeletePostConfirmModal> deletePost error handling

--- components/Modal/DeletePostConfirmModal/index.tsx
+++ components/Modal/DeletePostConfirmModal/index.tsx
@@ -12,9 +12,50 @@ const SignOutConfirmModal: FC<Props> = ({ deleteModal }) => {
   const { pid } = router.query;
 
   const confirmDeletePost = () => {
-    deletePost(pid);
+    deletePost(pid).then(
+      (res) => {
+        router.push("/");
+        Promise.resolve(res);
+      },
+      (err) => {
+        switch (err.response.status) {
+          case 400: {
+            switch (err.response.data.code) {
+              case "AUTHOR_MISMATCH":
+                alert("자신이 작성한 게시물이 아닙니다.");
+                break;
+              default:
+                break;
+            }
+          }
+          case 401: {
+            switch (err.response.data.code) {
+              case "TOKEN_EXPRIATION":
+                alert("만료된 토큰입니다.");
+                break;
+              case "INVALID_TOKEN":
+                alert("유효하지 않은 토큰입니다.");
+                break;
+              default:
+                break;
+            }
+          }
+          case 404: {
+            switch (err.response.data.code) {
+              case "USER_NOT_FOUND":
+                alert("존재하지 않는 사용자입니다.");
+                break;
+              default:
+                break;
+            }
+          }
+          default:
+            break;
+        }
+        Promise.reject(err);
+      }
+    );
     deleteModal();
-    router.push("/");
     return;
   };
 
