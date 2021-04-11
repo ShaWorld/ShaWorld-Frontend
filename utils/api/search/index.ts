809834efeab67c79c4cb95f238ba4708e75333e0
Sonwanseo <sonwanseo@gmail.com>
2021-04-11 23:59:22 +0900

    <utils/api/search> change interface name

--- utils/api/search/index.ts
+++ utils/api/search/index.ts
@@ -1,16 +1,16 @@
 import axios from "axios";
 import { BASE_URL, TOKEN_NAME } from "../BASE";
-import { GetLatestPosts } from "./payload";
+import { SearchLatestPosts } from "./payload";
 
 export const searchLatestPosts = async (
   page: number,
   size: number,
   keyword: string | string[]
-): Promise<GetLatestPosts[]> => {
+): Promise<SearchLatestPosts[]> => {
   const {
     data: { content },
   } = await axios.get<{
-    content: GetLatestPosts[];
+    content: SearchLatestPosts[];
   }>(BASE_URL + `/post/search?page=${page}&size=${size}&keyword=${keyword}`, {
     headers: {
       Authorization: localStorage.getItem(TOKEN_NAME),
