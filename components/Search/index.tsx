496a7d101d91b45bedb187f7d0128b4f1c5cf28f
Sonwanseo <sonwanseo@gmail.com>
2021-04-11 23:54:22 +0900

    <components/Search> Create Search component

--- components/Search/index.tsx
+++ components/Search/index.tsx
@@ -1,8 +1,119 @@
-import { FC } from "react";
+import { useRouter } from "next/router";
+import { FC, useEffect, useState } from "react";
+import PostItem from "../common/PostItem";
+import Header from "../Header";
+import { GetLatestPosts } from "../../utils/api/search/payload";
+import { searchLatestPosts } from "../../utils/api/search";
 import * as S from "./style";
+import useSearch from "../../utils/hooks/search";
 
 const Search: FC = () => {
-  return <S.Container>Search</S.Container>;
+  let page = 0;
+
+  const router = useRouter();
+  const [postList, setPostList] = useState<GetLatestPosts[]>([]);
+  const [fetching, setFetching] = useState<boolean>(false);
+  const [isEmpty, setIsEmpty] = useState<boolean>(false);
+  const {
+    state: { keyword },
+  } = useSearch();
+
+  useEffect(() => {
+    if (!keyword) {
+      router.push("/");
+      return;
+    }
+    serachLatestPostsClient();
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [keyword]);
+
+  const serachLatestPostsClient = () => {
+    setFetching(true);
+    searchLatestPosts(page++, 12, keyword).then(
+      (res) => {
+        console.log(res.length === 0);
+        if (res.length === 0) {
+          setIsEmpty(true);
+          setPostList([]);
+        } else {
+          setIsEmpty(false);
+          setPostList((prev) => prev.concat(res));
+        }
+        Promise.resolve(res);
+      },
+      (err) => {
+        switch (err.response.status) {
+          case 401: {
+            switch (err.response.data.code) {
+              case "TOKEN_EXPRIATION":
+                alert("만료된 토큰입니다.");
+                break;
+              case "INVALID_TOKEN":
+                alert("유효하지 않은 토큰입니다.");
+                break;
+              default:
+                break;
+            }
+          }
+          case 404: {
+            switch (err.response.data.code) {
+              case "USER_NOT_FOUND":
+                alert("존재하지 않는 사용자입니다.");
+                break;
+              default:
+                break;
+            }
+          }
+          default:
+            break;
+        }
+        Promise.reject(err);
+      }
+    );
+    setFetching(false);
+  };
+
+  const handleScroll = () => {
+    const scrollHeight = document.documentElement.scrollHeight;
+    const scrollTop = document.documentElement.scrollTop;
+    const clientHeight = document.documentElement.clientHeight;
+    if (scrollTop + clientHeight >= scrollHeight && fetching === false) {
+      serachLatestPostsClient();
+    }
+  };
+
+  return (
+    <S.Container>
+      <Header />
+      <S.Wrapper>
+        <S.Title>'{keyword}' 검색 결과</S.Title>
+        {isEmpty ? (
+          <S.EmptyTextWrapper>
+            <S.EmptyText>검색 결과가 없습니다.</S.EmptyText>
+          </S.EmptyTextWrapper>
+        ) : (
+          <S.PostWrapper>
+            {postList != []
+              ? postList.map((item) => (
+                  <PostItem
+                    key={item.postId}
+                    id={item.postId}
+                    thumbnail={item.postThumbnail}
+                    title={item.postTitle}
+                    price={item.postPrice}
+                    nickname={item.postAuthor}
+                    address={item.postAddress}
+                  />
+                ))
+              : ""}
+          </S.PostWrapper>
+        )}
+      </S.Wrapper>
+    </S.Container>
+  );
 };
 
 export default Search;
--- components/Search/style.tsx
+++ components/Search/style.tsx
@@ -1,3 +1,55 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Wrapper = styled.div`
+  width: 1200px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const Title = styled.h1`
+  margin: 0;
+  margin-left: 71px;
+  margin-top: 53px;
+  font-size: 30px;
+  font-weight: bold;
+  color: #807777;
+`;
+
+export const EmptyTextWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const EmptyText = styled.h1`
+  font-size: 50px;
+  color: #4f4646;
+`;
+
+export const PostWrapper = styled.div`
+  width: 1040px;
+  margin-top: 35px;
+  margin-left: 86px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+
+  &::after {
+    content: "";
+    flex-basis: 0;
+    flex-grow: 1;
+    flex-wrap: 1;
+  }
+`;
