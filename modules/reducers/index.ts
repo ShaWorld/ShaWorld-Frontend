a7b73a545faf9681cd05959d8511c01a88736bea
Sonwanseo <sonwanseo@gmail.com>
2021-04-11 23:55:15 +0900

    <modules/reducers> combine searchReducer

--- modules/reducers/index.ts
+++ modules/reducers/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import modalReducer from "./modal";
 import userReducer from "./user";
+import searchReducer from "./search";
 
 const rootReducer = combineReducers({
   modal: modalReducer,
   user: userReducer,
+  search: searchReducer,
 });
 
 export default rootReducer;
