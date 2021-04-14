bb5a9156105fb140852aa63b2c0707227ea624eb
Sonwanseo <sonwanseo@gmail.com>
2021-04-14 23:56:43 +0900

    <ApplyRentalConfigmModal/index.tsx> add server connection api

--- components/Modal/ApplyRentalConfirmModal/index.tsx
+++ components/Modal/ApplyRentalConfirmModal/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import { useRouter } from "next/router";
 import * as S from "./style";
+import { applyPost } from "../../../utils/api/postdetail";
 
 interface Props {
   deleteModal: () => void;
@@ -8,51 +9,52 @@ interface Props {
 
 const SignOutConfirmModal: FC<Props> = ({ deleteModal }) => {
   const router = useRouter();
+  const { pid } = router.query;
 
   const confirmApplyRental = () => {
-    // deletePost(pid).then(
-    //   (res) => {
-    //     router.push("/");
-    //     Promise.resolve(res);
-    //   },
-    //   (err) => {
-    //     switch (err.response.status) {
-    //       case 400: {
-    //         switch (err.response.data.code) {
-    //           case "AUTHOR_MISMATCH":
-    //             alert("자신이 작성한 게시물이 아닙니다.");
-    //             break;
-    //           default:
-    //             break;
-    //         }
-    //       }
-    //       case 401: {
-    //         switch (err.response.data.code) {
-    //           case "TOKEN_EXPRIATION":
-    //             alert("만료된 토큰입니다.");
-    //             break;
-    //           case "INVALID_TOKEN":
-    //             alert("유효하지 않은 토큰입니다.");
-    //             break;
-    //           default:
-    //             break;
-    //         }
-    //       }
-    //       case 404: {
-    //         switch (err.response.data.code) {
-    //           case "USER_NOT_FOUND":
-    //             alert("존재하지 않는 사용자입니다.");
-    //             break;
-    //           default:
-    //             break;
-    //         }
-    //       }
-    //       default:
-    //         break;
-    //     }
-    //     Promise.reject(err);
-    //   }
-    // );
+    applyPost(pid).then(
+      (res) => {
+        Promise.resolve(res);
+      },
+      (err) => {
+        switch (err.response.status) {
+          case 401: {
+            switch (err.response.data.code) {
+              case "TOKEN_EXPRIATION":
+                alert("만료된 토큰입니다.");
+                break;
+              case "INVALID_TOKEN":
+                alert("유효하지 않은 토큰입니다.");
+                break;
+              default:
+                break;
+            }
+          }
+          case 404: {
+            switch (err.response.data.code) {
+              case "USER_NOT_FOUND":
+                alert("존재하지 않는 사용자입니다.");
+                break;
+              default:
+                break;
+            }
+          }
+          case 409: {
+            switch (err.response.data.code) {
+              case "APPLY_DUPLICATION":
+                alert("이미 신청했습니다.");
+                location.reload();
+                break;
+              default:
+                break;
+            }
+          }
+          default:
+            break;
+        }
+        Promise.reject(err);
+      }
+    );
     deleteModal();
     router.push('/');
     return;
